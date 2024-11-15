17ff20941f848ddb5fb920550007e1bdae1dcfe2
Levi Nóbrega <levinobregab@gmail.com>
2024-11-15 12:13:34 -0300

    feat: intro to 0.0.2 Quoti Piece

---
+++ packages/pieces/community/quoti/src/index.ts
@@ -0,0 +1,65 @@
+
+    import { createPiece, PieceAuth, Property } from "@activepieces/pieces-framework";
+    import { getDatabase } from "./lib/actions/get-database";
+    import { listDatabase } from "./lib/actions/list-database";
+    import { updateDatabase } from "./lib/actions/update-database";
+    import {deleteDatabaseItem} from './lib/actions/delete-database';
+    import {createDatabaseItem} from './lib/actions/create-database';
+    import { databaseTrigger } from "./lib/triggers/database-trigger";
+    import { createCustomApiCallAction } from '@activepieces/pieces-common';
+    export type QuotiAuthType = { org_slug: string; BearerStatic: string };
+    
+    export const quotiAuth = PieceAuth.CustomAuth({
+      description: 'Enter Quoti Auth authentication details',
+      props: {
+          org_slug: Property.ShortText({
+              displayName: 'Org Slug',
+              description: 'Enter the base organization info present on your URL, like: https://quoti.cloud/$ORG_SLUG$/',
+              required: true,
+          }),
+          BearerStatic: PieceAuth.SecretText({
+              displayName: 'BearerStatic',
+              description: 'Enter the access token',
+              required: true
+          })
+      },
+      // Optional Validation
+      validate: async ({auth}) => {
+          if(auth.BearerStatic.length > 0 && auth.org_slug.length > 0){
+              return {
+                  valid: true,
+              }
+          }
+          return {
+              valid: false,
+              error: 'Invalid Auth'
+          }
+      },
+      required: true
+  })
+    
+    export const quoti = createPiece({
+      displayName: "Quoti",
+      auth: quotiAuth,
+      minimumSupportedRelease: '0.20.0',
+      logoUrl: "https://storage.googleapis.com/beyond-quoti-users-files/Quoti.svg",
+      authors: [],
+      actions: [listDatabase, getDatabase, createDatabaseItem, updateDatabase, deleteDatabaseItem,
+        createCustomApiCallAction({
+          // The auth object defined in the piece
+          auth: quotiAuth,
+          // The base URL for the API
+          baseUrl: (auth: any) => {
+            return `https://api.quoti.cloud/api/v1/${auth.org_slug}`
+          },
+          // Mapping the auth object to the needed authorization headers
+          authMapping: async (auth: any) => {
+            return {
+              'BearerStatic': `${auth.BearerStatic.BearerStatic}`
+            }
+          }
+        })
+      ],
+      triggers: [databaseTrigger],
+    });
+    
---
+++ packages/pieces/community/quoti/src/lib/actions/create-database.ts
@@ -0,0 +1,78 @@
+import {
+  createAction,
+  Property,
+  PieceAuth,
+  Validators,
+  DynamicPropsValue
+} from '@activepieces/pieces-framework';
+import { httpClient, HttpMethod } from '@activepieces/pieces-common';
+import {databasesDropdown, filterField, databaseAllFieldsForm} from '../common'
+import { quotiAuth, QuotiAuthType } from '../..';
+import axios from 'axios';
+type QueryParams = {
+  [key: string]: string | object; // Index signature
+};
+
+export const createDatabaseItem = createAction({
+  // auth: check https://www.activepieces.com/docs/developers/piece-reference/authentication,
+  name: 'createDatabaseItem',
+  auth: quotiAuth,
+  displayName: 'Create on database',
+  description: 'Create a specific item on a database',
+  props: {
+    databaseSlug: databasesDropdown,
+    fields: databaseAllFieldsForm,
+    hasAdvancedProps: Property.Checkbox({
+      displayName: 'Advanced',
+      description: 'Check this box to show advanced configuration',
+      required: false,
+      defaultValue: false,
+    }),
+    advancedProps: Property.DynamicProperties({
+      description: 'Dynamic Form',
+      displayName: 'Dynamic Form',
+      required: true,
+      refreshers: ['hasAdvancedProps'],
+      props: async (propsValue) => {
+        // const authentication = propsValue['authentication'];
+        const hasAdvancedProps = propsValue['hasAdvancedProps']
+        if(hasAdvancedProps) {
+          const properties = {
+            json: Property.Json({
+              displayName: 'Body JSON',
+              description: 'If you use this option all previous settings will be OVERWRITE.',
+              required: false,
+              defaultValue: {"a": "b", "date": "2024-22-01"},
+            })
+          };
+      
+          return properties;
+        } else return {
+        }
+      },
+    })
+    
+    },
+  async run(context) {
+    let body: QueryParams = {
+    }
+    if(context.propsValue['hasAdvancedProps'] && Object.keys(context.propsValue['advancedProps']['json']).length > 0 ){
+      body = context.propsValue['advancedProps']['json']
+    } else {
+      if(context.propsValue['fields'] && Object.keys(context.propsValue['fields']).length > 0){
+        for (const prop of Object.keys(context.propsValue['fields'])){
+          if(context.propsValue['fields'][prop]){
+            body[prop] = context.propsValue['fields'][prop] 
+          }
+        }
+      }
+    }
+    console.log('BODY', JSON.stringify(body))
+    const res = await axios.post(`https://api.quoti.cloud/api/v1/${context.auth.org_slug}/resources/${context.propsValue['databaseSlug']}`,body, {
+      headers: {
+        BearerStatic: context.auth.BearerStatic, // Pass API key in headers
+      }
+    })
+    return res.data;
+  },
+});
